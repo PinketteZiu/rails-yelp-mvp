1f0c87fc6627cfa4b0bb37ff10e0ad2a826cdd08
Julie Veran <julieveran31@gmail.com>
2024-11-13 16:38:46 +0100

    add Yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [ :create]
+
+  def create
+    @review = Review.new(restaurant_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant_id), status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
